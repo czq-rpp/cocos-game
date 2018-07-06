0d9593a6df5c3a447049509f2f81a1c96566b272
zaccheus <1522980087@qq.com>
2018-07-06 10:11:55 +0800

    mocha工具

---
+++ majiang/mocha/binarySearch.js
@@ -0,0 +1,51 @@
+/*
+ * @Author: zaccheus 
+ * @Date: 2018-07-06 09:05:47 
+ * @Last Modified by: zaccheus
+ * @Last Modified time: 2018-07-06 10:04:20
+ */
+
+class BinarySearch {
+  constructor() {
+    this.needSearchSortedArray = null;
+    this.needFindVal = null;
+  }
+  /**
+   * 
+   * @param {*} fromIndex 
+   * @param {*} toIndex 
+   */
+  searchScope(fromIndex, toIndex) {
+    if(fromIndex > toIndex) {
+      return 0;
+    }
+    var centerIndex = fromIndex + parseInt((toIndex - fromIndex) / 2);
+    var centerVal = this.needSearchSortedArray[centerIndex];
+
+    if(centerVal === this.needFindVal) {
+      return 1;
+    }
+
+    if(centerVal > this.needFindVal) {
+      this.searchScope(centerIndex + 1, toIndex);
+    }
+
+    return this.searchScope(fromIndex, centerIndex - 1);
+  }
+  /**
+   * 
+   * @param {*} sortedArray 
+   * @param {*} findVal 
+   * @returns -1表示传递的值有问题  1表示找到元素了  0表示没有找到
+   */
+  search(sortedArray, findVal) {
+    this.needSearchSortedArray = sortedArray;
+    this.needFindVal = findVal;
+    if (sortedArray === null || typeof sortedArray === "undefined" || !(sortedArray instanceof Array)) {
+      return -1;
+    }
+    return this.searchScope(0, sortedArray.length - 1);
+  }
+}
+
+module.exports = BinarySearch;
---
+++ majiang/mocha/binarySearch.test.js
@@ -0,0 +1,47 @@
+/*
+ * @Author: zaccheus 
+ * @Date: 2018-07-06 09:47:43 
+ * @Last Modified by: zaccheus
+ * @Last Modified time: 2018-07-06 10:10:06
+ */
+
+var BinarySearch = require("./binarySearch");
+var Assert = require("assert");
+
+describe('测试二分查找算法', function() {
+  describe('测试传递null值', function() {
+    it('必须返回-1', function() {
+      var binarySearchInstance = new BinarySearch();
+      var ok = binarySearchInstance.search(null, 0);
+      Assert.equal(-1, ok);
+    });
+  });
+  describe('测试传递undefined值', function() {
+    it('必须返回-1', function() {
+      var binarySearchInstance = new BinarySearch();
+      var ok = binarySearchInstance.search(undefined, 0);
+      Assert.equal(-1, ok);
+    });
+  });
+  describe('测试传递非数组值', function() {
+    it('必须返回-1', function() {
+      var binarySearchInstance = new BinarySearch();
+      var ok = binarySearchInstance.search("sadas", 0);
+      Assert.equal(-1, ok);
+    });
+  });
+  describe('测试传递数组，且能找到', function() {
+    it('必须返回1', function() {
+      var binarySearchInstance = new BinarySearch();
+      var ok = binarySearchInstance.search([1,45,78,90,100], 78);
+      Assert.equal(1, ok);
+    });
+  });
+  describe('测试传递数组，且不能找到', function() {
+    it('必须返回0', function() {
+      var binarySearchInstance = new BinarySearch();
+      var ok = binarySearchInstance.search([1,45,78,90,100], 178);
+      Assert.equal(0, ok);
+    });
+  });
+});
